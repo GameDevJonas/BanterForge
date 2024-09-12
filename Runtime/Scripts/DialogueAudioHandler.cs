eb42264c78f9504cb045a0086c8619b8af8423e7
Jonas <oddenjonas1@gmail.com>
2024-09-12 20:25:08 +0200

    1.0.4 - Made audio handler class entirely abtract

--- Runtime/Scripts/DialogueAudioHandler.cs
+++ Runtime/Scripts/DialogueAudioHandler.cs
@@ -4,26 +4,14 @@ namespace GDPanda.BanterForge
 {
     public abstract class DialogueAudioHandler : MonoBehaviour
     {
-        private bool ContainsSpecialLetter(char letter)
+        public bool ContainsSpecialLetter(char letter)
         {
             return letter is ' ' or ',' or '=' or '*' or '!' or ':' or ')' or '.';
         }
 
-        public virtual void PlayCharacterRevealAudio(char character)
-        {
-            string charEvent = "event:/Dialogue/" + character;
-            if (ContainsSpecialLetter(character))
-                charEvent = "event:/Dialogue/space";
-            
-            //Run audio event
-        }
+        public abstract void PlayCharacterRevealAudio(char character);
 
-        public virtual void PlayOnEmotionChangedAudio(string emotion)
-        {
-            string emotionEvent = "event:/SetMood/Set" + emotion;
-            
-            //Run audio event
-        }
+        public abstract void PlayOnEmotionChangedAudio(string emotion);
         
         public abstract void SetAudioParamaterByName(string character, int currentSpeakerVoiceType);
         
